30ee32ac31a070db631a2d2d20291647b1781047
sushant-m-dev <mhambrey.sushant@gmail.com>
2023-10-09 22:47:22 -0500

    sending 100 simaltenous files

--- demo/string_test.py
+++ demo/string_test.py
@@ -2,25 +2,16 @@ import concurrent.futures
 import socket
 import time
 
-# Function to send a string over a socket connection
-def send_string(data):
+# Function to send a string and output file information over a socket connection
+def send_string_and_file(data, output_file):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(("127.0.0.1", 9988))
-        s.sendall(data.encode())
+        s.sendall(f"{data}:{output_file}".encode())
 
-# List of 10 strings with longer text to send
-strings_to_send = [
-    "tts:This is a longer text for string 1. It contains more words and characters.",
-    "tts:Another longer text for string 2 with even more content and details.",
-    "tts:Here's a lengthy text for string 3 that goes on and on with lots of information.",
-    "tts:String 4 has a lengthy paragraph with additional information and details.",
-    "tts:This is a lengthy text for string 5. It's quite long and comprehensive.",
-    "tts:Another extensive text for string 6 with lots of content and explanations.",
-    "tts:Text for string 7 contains a substantial amount of information and details.",
-    "tts:String 8 has a long paragraph with additional context and explanations.",
-    "tts:This is an extensive text for string 9. It's quite long and informative.",
-    "tts:Text for string 10 is lengthy, containing a wealth of information and content."
-]
+# Generate a list of 100 strings with longer text to send
+strings_to_send = []
+for i in range(1, 101):
+    strings_to_send.append(f"This is string {i}. It contains more words and characters.")
 
 # Create a ThreadPoolExecutor with a maximum of 10 worker threads
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
@@ -28,6 +19,6 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     for i, string in enumerate(strings_to_send):
         # Modify the filename based on some criteria (e.g., index)
         output_file = f'/root/sushant/seamless_m4t/seamless_communication/demo/output_{i}.wav'
-        executor.submit(send_string, string)
+        executor.submit(send_string_and_file, string, output_file)
         # Sleep for a moment to ensure unique filenames if needed
         time.sleep(0.1)  # Adjust the sleep time as needed
