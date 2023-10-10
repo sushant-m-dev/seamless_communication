00ebec9322df4d5dd0dd6dfc58ff62c9495a5e84
sushant-m-dev <mhambrey.sushant@gmail.com>
2023-10-09 22:51:15 -0500

    remove output file from client side

--- demo/string_test.py
+++ demo/string_test.py
@@ -3,7 +3,7 @@ import socket
 import time
 
 # Function to send a string and output file information over a socket connection
-def send_string_and_file(data, output_file):
+def send_string_and_file(data):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(("127.0.0.1", 9988))
         s.sendall(f"{data}:{output_file}".encode())
@@ -19,6 +19,6 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     for i, string in enumerate(strings_to_send):
         # Modify the filename based on some criteria (e.g., index)
         output_file = f'/root/sushant/seamless_m4t/seamless_communication/demo/output_{i}.wav'
-        executor.submit(send_string_and_file, string, output_file)
+        executor.submit(send_string_and_file, string)
         # Sleep for a moment to ensure unique filenames if needed
         time.sleep(0.1)  # Adjust the sleep time as needed
